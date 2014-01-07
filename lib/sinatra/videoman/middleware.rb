b9778bc70b4737a12176424490a01dab1568b6c0
Dobromir Ivanov <dobromir0ivanov@gmail.com>
2014-01-07 00:55:18 +0000

    Middleware is now a module and it does not subclass Sinatra::Base, because we do not want to use this gem as a middleware.

--- lib/sinatra/videoman/middleware.rb
+++ lib/sinatra/videoman/middleware.rb
@@ -1,83 +1,93 @@
-require 'sinatra/partial'
-
 module Sinatra
   module Videoman
-    class Middleware < Sinatra::Base
+    module Middleware
       include Manager
-      register Sinatra::Partial
-      set :partial_template_engine, :erb
+      def self.registered(app)
+        app.register Sinatra::Partial
+        app.set :partial_template_engine, :erb
 
-      get '/videos/upload/?' do
-        erb :upload
-      end
+        app.get '/videos/upload/?' do
+          erb :upload
+        end
 
-      post '/videos/upload' do
-        video = Video.new(params[:video])
-        params[:video_files].each do |file|
-          file = VideoFile.new(:file => file, :content_type => file[:type])
-          if file.valid?
-            video.video_files << file
-            file.save!
-            Manager.call :after_file_upload, [file]
+        app.post '/videos/upload' do
+          video = Video.new(params[:video])
+          params[:video_files].each do |file|
+            file = VideoFile.new(:file => file, :content_type => file[:type])
+            if file.valid?
+              video.video_files << file
+              file.save!
+              Manager.call :after_file_upload, [file]
+            else
+              Manager.call :after_file_upload_failure, [request, response]
+              flash[:error] = file.errors.messages
+              redirect '/videos/upload'
+            end
+          end
+          if video.valid? && video.video_files.size >= 1
+            video.save!
+            Manager.call :after_video_save, [video, request, response]
+            flash[:notice] = Manager.config[:after_video_save_msg]
+            redirect Manager.config[:after_video_save_path]
           else
-            Manager.call :after_file_upload_failure, [request, response]
-            flash[:error] = file.errors.messages
+            Manager.call :after_video_save_failure, [request, response]
+            flash[:error] = video.errors.messages
             redirect '/videos/upload'
           end
         end
-        if video.valid? && video.video_files.size >= 1
-          video.save!
-          Manager.call :after_video_save, [video, request, response]
-          flash[:notice] = Manager.config[:after_video_save_msg]
-          redirect Manager.config[:after_video_save_path]
-        else
-          Manager.call :after_video_save_failure, [request, response]
-          flash[:error] = video.errors.messages
-          redirect '/videos/upload'
+
+        app.get '/videos/show/:id/?' do
+          @video = Video.find_by id: params[:id]
+          if @video
+            erb :show
+          else
+            'No video found'
+          end
         end
-      end
 
-      get '/videos/show/:id/?' do
-        @video = Video.find(params[:id])
-        erb :show
-      end
-
-      get '/videos/edit/:id/?' do
-        @video = Video.find(params[:id])
-        erb :edit
-      end
-
-      post '/videos/edit/:id' do
-        video = Video.find(params[:id])
-        video.update_atributes(params)
-        if video.valid?
-          video.save
-          Manager.call :after_video_update, [video, request, response]
-          flash[:notice] = Manager.config[:after_video_update_msg]
-          redirect Manager.config[:after_video_update_path]
-        else
-          Manager.call :after_video_update_failure, [video, request, response]
-          flash[:error] = video.errors.messages
-          redirect Manager.config[:after_video_update_failure_path]
+        app.get '/videos/edit/:id/?' do
+          @video = Video.find_by id: params[:id]
+          if @video
+            erb :edit
+          else
+            'No video found'
+          end
         end
-      end
 
-      post '/videos/delete/:id' do
-        video = Video.find(params[:id])
-        Manager.call :before_video_delete, [video, request, response]
-        if video
-          video.thumbnail.remove!
-          video.video_files.each {|file| file.file.remove!}
-          video.delete
-          Manager.call :after_video_delete, [request, response]
-          flash[:notice] = Manager.config[:after_video_delete_msg]
+        app.post '/videos/edit/:id' do
+          video = Video.find(params[:id])
+          if video.update_attributes(params)
+            video.save
+            Manager.call :after_video_update, [video, request, response]
+            flash[:notice] = Manager.config[:after_video_update_msg]
+            redirect Manager.config[:after_video_update_path]
+          else
+            Manager.call :after_video_update_failure, [video, request, response]
+            flash[:error] = video.errors.messages
+            redirect Manager.config[:after_video_update_failure_path]
+          end
         end
-        redirect Manager.config[:after_video_delete_path]
-      end
 
-      get '/videos/' do
-        @videos = Video.all
-        erb :index
+        app.post '/videos/delete/:id' do
+          video = Video.find_by id: params[:id]
+          if video
+            Manager.call :before_video_delete, [video, request, response]
+            video.thumbnail.remove!
+            video.video_files.each  do |file|
+              file.file.remove!
+              file.destroy!
+            end
+            video.destroy!
+            Manager.call :after_video_delete, [request, response]
+            flash[:notice] = Manager.config[:after_video_delete_msg]
+          end
+          redirect Manager.config[:after_video_delete_path]
+        end
+
+        app.get '/videos/?' do
+          @videos = Video.all
+          erb :index
+        end
       end
     end
   end
